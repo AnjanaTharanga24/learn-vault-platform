ab80474c8c87dac68884471c5470c7dd5a633512
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2025-03-27 14:31:37 +0530

    create right sidebar

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Sidebar from '../../components/Sidebar/Sidebar'
+import Sidebar2 from '../../components/Sidebar/Sidebar2'
 
 export default function Home() {
   return (
-    <div className='p-4 mt-5'>
-        <Navbar/>
-        <Sidebar/>
-      <h1>this is home</h1>
+    <div className='container-fluid p-0'>
+      <Navbar/>
+      <div className="row g-0">
+        <div className="col-lg-2">
+          <Sidebar/>
+        </div>
+        <div className="col-lg-8 main-content">
+          <div className="container-fluid p-4">
+            <h1>Welcome to SkillShare</h1>
 
-      
-
-      
+            {Array.from({ length: 20 }).map((_, index) => (
+              <p key={index}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            ))}
+          </div>
+        </div>
+        <div className="col-lg-2">
+          <Sidebar2/>
+        </div>
+      </div>
     </div>
   )
 }
